5ea91aaa7f26504e6668dd25fdf442d420278065
Ben Frengley <b3nf@hotmail.com>
2016-04-13 16:45:43 +1200

    Initial commit

---
+++ src/set1.rs
@@ -0,0 +1,32 @@
+fn hex_string_to_bytes(hex_str: &str) -> Option<Vec<u8>> {
+    let mut bytes: Vec<u8> = Vec::new();
+    let mut byte = 0u8;
+
+    for (i, b) in hex_str.chars().enumerate() {
+        match b.to_digit(16) {
+            Some(n) => byte += (n << (4 * (1 - (i % 2)))) as u8,
+            None => return None,
+        }
+
+        if i % 2 == 0 || i == hex_str.len() - 1 {
+            bytes.push(byte);
+            byte = 0;
+        }
+    }
+
+    Some(bytes)
+}
+
+// fn hex_to_base64(hex_str: &str) -> String {
+//     let hex = 
+// }
+
+#[cfg(test)]
+mod test {
+    use super::hex_string_to_bytes;
+
+    #[test]
+    fn test_hex_string_to_bytes() {
+        assert_eq!(vec![1u8], hex_string_to_bytes("01").unwrap());
+    }
+}
